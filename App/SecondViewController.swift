a30b3ee81d89599901e6cf69247ddce7c689eeaa
Administrador <administrador@SP06.local>
2016-04-21 19:43:57 -0300

    Realizado el TableViewCell

--- App/SecondViewController.swift
+++ App/SecondViewController.swift
@@ -8,17 +8,52 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableViewTest: UITableView!
+    
+    let textCellIdentifier = "TableViewCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableViewTest.delegate = self
+        tableViewTest.dataSource = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @available(iOS 2.0, *)
+    internal func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 10 //apps.count
+        
+    }
+    
+    
+    
+    @available(iOS 2.0, *)
+    internal func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as! TableViewCell
+        
+        
+        cell.profileName?.text = "Hola" //apps[indexPath].profileName
+        cell.profileDescription?.text = "Descripcion"
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.performSegueWithIdentifier("VerCupon", sender: nil)
+    }
+
 
 
 }
---
+++ App/TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewCell.swift
+//  App
+//
+//  Created by Administrador on 21/4/16.
+//  Copyright © 2016 DLA. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var profileDescription: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
