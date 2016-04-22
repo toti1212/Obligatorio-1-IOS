a15bd304317839b020e73313fe06ba9eb317b223
Rodrigo Suarez Balbi <rodrigosrz12@gmail.com>
2016-04-22 20:17:18 -0300

    Integracion con cocoapods. Generacion de codigo QR en base a un string dado

--- App/FirstViewController.swift
+++ App/FirstViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import QRCode
 
 class FirstViewController: UIViewController {
+    
+
+    @IBOutlet weak var qrImageView: UIImageView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        qrImageView.image = {
+            var qrCode = QRCode("ucu@ucu.com")!
+            qrCode.size = self.qrImageView.bounds.size
+            qrCode.errorCorrection = .High
+            return qrCode.image
+            }()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
